8d715c7d508c1b43cc03c1e6f5736d2f374bef5b
Damian Gola <damian.gola@gmail.com>
2015-08-16 12:08:25 +0200

    added units
    still need to add scopecurator

--- aftp_c_units_aftp/config.cpp
+++ aftp_c_units_aftp/config.cpp
@@ -18,7 +18,7 @@ class CfgPatches {
 	class aftp_c_units_aftp {
 		version = 1;
 		author = "AFTP Golas & Waffel";
-		units[] = {};
+		units[] = {"aftp_01_wz93", "aftp_01_tl_wz93", "aftp_01_gr_wz93", "aftp_01_ar_wz93", "aftp_sf_wz93", "aftp_02_wz93", "aftp_03_wz93", "aftp_04_wz93", "aftp_05_wz93", "aftp_01_wzd93", "aftp_01_tl_wzd93", "aftp_01_gr_wzd93", "aftp_01_ar_wzd93", "aftp_sf_wzd93", "aftp_02_wzd93", "aftp_03_wzd93", "aftp_04_wzd93", "aftp_05_wzd93", "aftp_01_mc", "aftp_01_tl_mc", "aftp_01_gr_mc", "aftp_01_ar_mc", "aftp_sf_mc", "aftp_02_mc", "aftp_03_mc", "aftp_04_mc", "aftp_05_mc", , "aftp_01_mct", "aftp_01_tl_mct", "aftp_01_gr_mct", "aftp_01_ar_mct", "aftp_sf_mct", "aftp_02_mct", "aftp_03_mct", "aftp_04_mct", "aftp_05_mct", "aftp_01_mca", "aftp_01_tl_mca", "aftp_01_gr_mca", "aftp_01_ar_mca", "aftp_sf_mca", "aftp_02_mca", "aftp_03_mca", "aftp_04_mca", "aftp_01_aau", "aftp_01_tl_aau", "aftp_01_gr_aau", "aftp_01_ar_aau", "aftp_sf_aau", "aftp_02_aau", "aftp_03_aau", "aftp_04_aau", "aftp_05_aau", "aftp_01_afg", "aftp_01_tl_afg", "aftp_01_gr_afg", "aftp_01_ar_afg", "aftp_sf_afg", "aftp_02_afg", "aftp_03_afg", "aftp_04_afg", "aftp_05_afg", "aftp_01_ale", "aftp_01_tl_ale", "aftp_01_gr_ale", "aftp_01_ar_ale", "aftp_sf_ale", "aftp_02_ale", "aftp_03_ale", "aftp_04_ale"};
 		weapons[] = {};
 		requiredVersion = 0.1;
 		requiredAddons[] = {"A3_Characters_F_BLUFOR", "A3_Characters_F_Common", "A3_Characters_F_Heads", "A3_Modules_F", A3_UI_F, "A3_Structures_F_Items_Electronics", "aftp_units"};
